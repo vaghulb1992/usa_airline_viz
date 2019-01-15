17af139e84423f0aff75c7c2fe54e9037f343b14
Rajeev Roy <rajeev.roy@gmail.com>
2019-01-14 18:06:53 -0800

    Initial R file

--- UnitedAirlinesAnalysis/united.R
+++ UnitedAirlinesAnalysis/united.R
@@ -13,5 +13,14 @@ agg_data<-join(agg_data, df)
 #Sort the data by Frequency
 agg_data<-agg_data[order(agg_data$freq),]
 agg_data$perFlightDelay<-agg_data$TotalDelay/agg_data$freq
-plot(agg_data$freq, agg_data$perFlightDelay)
-text(agg_data$freq, agg_data$perFlightDelay, agg_data$UniqueCarrier)
+plot(agg_data$freq, agg_data$perFlightDelay,
+     xlab="Number of Flights",
+     ylab="Average Delay per Flight (minutes)",
+     xlim=c(0,1300000), ylim=c(0,30))
+#text(agg_data$freq+50000, agg_data$perFlightDelay, agg_data$UniqueCarrier)
+com_data<-subset(agg_data, agg_data$UniqueCarrier!=c("UA","MQ", "DL", "US"))
+text(com_data$freq+50000, com_data$perFlightDelay, com_data$UniqueCarrier)
+title("Total Flight Delay Comparision")
+mtext('Benchmarking American Airlines', side=3, line=0)
+aa_data<-agg_data[agg_data$UniqueCarrier=="UA",]
+text(aa_data$freq +50000, aa_data$freq, "BA", col = "Red")
