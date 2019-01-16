d76d1c104723c730f3780618ff39b4893bd8e6e6
Vaghul Aditya Balaji <vaghulb1992@gmail.com>
2019-01-15 18:39:25 -0800

    Pushing in the final changes

--- airportRouteAnalysis/server.R
+++ airportRouteAnalysis/server.R
@@ -1,5 +1,6 @@
 library(leaflet)
 library(maps)
+library(DT)
 
 server <- function(input, output){
   output$airport_map <- renderLeaflet({
@@ -16,11 +17,44 @@ server <- function(input, output){
     airports <- airports[order(airports$propCancelled), ]
     cancellations <- cancellations[order(cancellations$propCancelled), ]
 
-    # subsetting again based on the user-inputted quantiles))
-    airports <- subset(airports, airports$propCancelled >= quantile(airports$propCancelled, input$airport_rank[1]) &
-                         airports$propCancelled <= quantile(airports$propCancelled, input$airport_rank[2]))
-    cancellations <- subset(cancellations, cancellations$propCancelled >= quantile(cancellations$propCancelled, input$route_rank[1]) &
-                              cancellations$propCancelled <= quantile(cancellations$propCancelled, input$route_rank[2]))
+    # preparing the datasets to display on the UI tables
+    ##### Best Airports
+    best_airports <- head(airports, 5)[, c('iata_code', 'propCancelled')]
+    best_airports$propCancelled <- round(best_airports$propCancelled, 4)
+    rownames(best_airports) <- best_airports$iata_code
+    best_airports$iata_code <- NULL
+    colnames(best_airports) <- c("Proportion of flights cancelled")
+    output$best_airports <- DT::renderDataTable({DT::datatable(best_airports, style = 'bootstrap', autoHideNavigation = FALSE,
+                                                               options = list(searching = FALSE, paging = FALSE,
+                                                                              ordering = FALSE, autoWidth = TRUE))})
+
+    ##### Worst Airports
+    worst_airports <- tail(airports, 5)[, c('iata_code', 'propCancelled')]
+    worst_airports <- worst_airports[order(worst_airports$propCancelled, decreasing = TRUE), ]
+    worst_airports$propCancelled <- round(worst_airports$propCancelled, 4)
+    rownames(worst_airports) <- worst_airports$iata_code
+    worst_airports$iata_code <- NULL
+    colnames(worst_airports) <- c("Proportion of flights cancelled")
+    output$worst_airports <- DT::renderDataTable({DT::datatable(worst_airports, style = 'bootstrap', autoHideNavigation = FALSE,
+                                                               options = list(searching = FALSE, paging = FALSE,
+                                                                              ordering = FALSE, autoWidth = TRUE))})
+
+    ##### Best Routes
+    best_routes <- head(cancellations, 5)[, 'propCancelled', drop = FALSE]
+    best_routes$propCancelled <- round(best_routes$propCancelled, 4)
+    colnames(best_routes) <- c("Proportion of flights cancelled")
+    output$best_routes <- DT::renderDataTable({DT::datatable(best_routes, style = 'bootstrap', autoHideNavigation = FALSE,
+                                                             options = list(searching = FALSE, paging = FALSE,
+                                                                            ordering = FALSE, autoWidth = TRUE))})
+
+    ##### Worst Routes
+    worst_routes <- tail(cancellations, 5)[, 'propCancelled', drop = FALSE]
+    worst_routes$propCancelled <- worst_routes[order(worst_routes$propCancelled, decreasing = TRUE), ]
+    worst_routes$propCancelled <- round(worst_routes$propCancelled, 4)
+    colnames(worst_routes) <- c("Proportion of flights cancelled")
+    output$worst_routes <- DT::renderDataTable({DT::datatable(worst_routes, style = 'bootstrap', autoHideNavigation = FALSE,
+                                                             options = list(searching = FALSE, paging = FALSE,
+                                                                            ordering = FALSE, autoWidth = TRUE))})
 
     # preparing a separate dataframe to be used by the addPolylines function later as it works in a specific way
     spatial_info <- data.frame(lat = numeric(), long = numeric(), group = numeric())
@@ -73,9 +107,9 @@ server <- function(input, output){
 
     # adding legends to make life easier
     map_leaflet <- addLegend(map_leaflet, "topright", pal = airport_pal, values = airports$propCancelled,
-                             title = "Cancellation Ratio per airport", opacity = 0.5)
+                             title = "Proportion of flights cancelled<br>per airport", opacity = 0.5)
     map_leaflet <- addLegend(map_leaflet, "bottomright", pal = journey_pal, values = cancellations$propCancelled,
-                             title = "Cancellation Ratio per route", opacity = 0.5)
+                             title = "Proportion of flights cancelled<br>per route", opacity = 0.5)
 
     # now we're done, time to display
     map_leaflet
--- airportRouteAnalysis/ui.R
+++ airportRouteAnalysis/ui.R
@@ -1,14 +1,25 @@
 library(leaflet)
+library(DT)
 
 ui <- fluidPage(
-  headerPanel("Which are the best/worst airports and flight routes?"),
+  headerPanel("Which are the best/worst airports and flight routes? (based on cancellations)"),
   leafletOutput("airport_map", height = 700),
   sidebarPanel(
-    sliderInput("airport_rank", "Enter desired airport rankings (quantile)", min=0, max=1, value=c(0.1, 0.6), step = 0.001),
-    sliderInput("dep_count", "Enter departure count cutoff", min=1, max=411964, value=c(3000, 300000))
+    sliderInput("dep_count", "No. of departures per airport", min=1, max=411964, value=c(3000, 300000)),
+    sliderInput("route_count", "No. of flights per route", min=1, max=13787, value=c(5000, 10000))
   ),
   sidebarPanel(
-    sliderInput("route_rank", "Enter desired route rankings (quantile)", min=0, max=1, value=c(0.1, 0.6), step = 0.001),
-    sliderInput("route_count", "Enter route count cutoff", min=1, max=13787, value=c(5000, 10000))
+    tabsetPanel(
+      id = 'dataset',
+      tabPanel("Best Airports", DT::dataTableOutput("best_airports")),
+      tabPanel("Worst Airports", DT::dataTableOutput("worst_airports"))
+    )
+  ),
+  sidebarPanel(
+    tabsetPanel(
+      id = 'dataset',
+      tabPanel("Best Flight Routes", DT::dataTableOutput("best_routes")),
+      tabPanel("Worst Flight Routes", DT::dataTableOutput("worst_routes"))
+    )
   )
 )
